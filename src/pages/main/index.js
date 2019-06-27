85e36ae3c371a7d613d9161761f6fa05bf4b5ca4
diegotesch <diego.tesch@gmail.com>
2019-06-27 16:04:44 -0300

    Armazenando no status

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component{
+    state = {
+        products: [],
+    }
+
     //Executa assim que o componente é mostrado na tela
     componentDidMount(){
         this.loadProducts();
@@ -10,10 +14,16 @@ export default class Main extends Component{
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
     };
 
     render(){
-        return <h1>Hello Galera</h1>
+        return (
+            <div className="product-list">
+                { this.state.products.map(product => (
+                    <h2 key={ product._id } >{ product.title }</h2>
+                ))}
+            </div>
+        )
     }
 }
